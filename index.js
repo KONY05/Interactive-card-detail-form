12c74d93fc8a91e1f2013ffffc3577716f125f54
konyinsola <konyinsolaodesanya@gmail.com>
2024-06-12 15:47:20 +0100

    finish creating js file

--- index.js
+++ index.js
@@ -10,30 +10,34 @@ const month = document.querySelector('.form--month');
 const year = document.querySelector('.form--year');
 const cvc = document.querySelector('.form--cvc');
 
-const form = [userName, cardNumber, month, year, cvc];
+const formInputs = [userName, cardNumber, month, year, cvc];
 
+const form = document.querySelector('.form-container');
 const btn = document.querySelector('.btn');
 const input = document.querySelectorAll('input');
 const label = document.querySelectorAll('label');
+const body = document.querySelector('body');
 
-const CARD_NUM_LEN = 19;
-const DATE_NUM_LEN = 2;
-const CVC_NUM_LEN = 3;
+const CARD_NUM_LEN = cardNumber.dataset.length;
+const DATE_NUM_LEN = month.dataset.length;
+const CVC_NUM_LEN = cvc.dataset.length;
 
 
+// ERROR HANDLING FUNCTIONS
 
 function displayErr(el, errMsg) {
-    //1) creating error message to append to element parent element
+    // creating error message to append to its parent element
     const p = document.createElement("p");
     p.textContent = `${errMsg}`;
     p.className = 'error-message';
 
-    //2) add error class and error message to element
+    // add error class and error message to element
     el.classList.add('error');
     el.parentNode.appendChild(p);
 }
 
 function removeErrorMessage(input) {
+    // getting the appended p tag
     const existingError = input.parentNode.querySelector('.error-message');
     // check if error message exists and removes it
     if (existingError) {
@@ -41,111 +45,175 @@ function removeErrorMessage(input) {
     }
 }
 
-function checkIfNumber(el, len) {
-    // check if element value is positive
-    if (el.value.length === len) {
-        // remove error class and error message if present
-        removeErrorMessage(el);
-        return el.classList.remove('error');
-    };
-
-    // check if element value is a string/not a number
-    if (el.value.length < len) {
-        // if current element does not contain the error class display error message
-        removeErrorMessage(el);
-        return displayErr(el, `${el.value.trim() === ''? "Can't be blank" : 'Wrong format, numbers only'}`);
-    }
-}
-
-function checkIfString(el) {
-    // remove error message if present
-    // check if element value is a number or is empty
-    if (Number.parseInt(el.value) || el.value.trim() === '') {
-        removeErrorMessage(el);
-        return displayErr(el, 'Please input your full name')
-    };
-
-    // check if element value is a string
-    const pattern = /^[A-Za-z]+ [A-Za-z]+$/;
-    if (pattern.test(el.value)){
-        removeErrorMessage(el);
-        return el.classList.remove('error');}
-}
-
-function renderCard(el, target) {
-    if (el.value.trim() === '' || el.value.length < 0) return;
-
-        el.value.trim();
-        target.textContent = '';
-        const text1 = el.value.slice(0, 1).toUpperCase();
-        const text2 = el.value.slice(1).toLowerCase();
-        const text = text1 + text2;
-        target.textContent = text;
-}
-
-function checkForErrorClass(el) {
-    if(el.classList.contains('error')) console.log('hello');
-}
-
 function formatCreditCardNumber() {
+    // replacing non number characters with empty
     let value = this.value.replace(/\D/g, '');
-
+    // matching Card Number with this regEx
     value = value.match(/.{1,4}/g)?.join(' ') || '';
 
     this.value = value;
 }
 
 function formatNumbers() {
+    // replacing non number characters with empty
     let value = this.value.replace(/\D/g, '');
     this.value = value;
 }
 
+function checkIfNumber(el, len) {
+    // el is already a number due to the formatting from previous function
+
+    // remove error class and error message if present
+    removeErrorMessage(el);
+    el.classList.remove('error');
+
+    // check if element value is the same lengt as the required length
+    if (el.value.length < len) {
+        // remove error class and error message if present and disp
+        removeErrorMessage(el);
+        // ternery operator for the error message for the input field if blank or not following the format
+        return displayErr(el, `${el.value.trim() === ''? "Can't be blank" : 'Wrong format, numbers only'}`);
+    }
+}
+
 function formatUsername() {
+    // replacing any whitespace at the beginning with empty and not allowing for any number
     let value = this.value.replace(/^[\s]/g, '');
-    value = value.replace(/[0-9]/g, '')
+    value = value.replace(/[0-9]/g, '');
     this.value = value;
 }
 
+function checkIfString(el) {
+    // check if element value is a number or is empty
+    if (Number.parseInt(el.value) || el.value.trim() === '') {
+        // remove error message if present and display new error
+        removeErrorMessage(el);
+        return displayErr(el, 'Please input your full name')
+    };
+
+    // check element value against the regEx
+    const pattern = /^[A-Za-z]+ [A-Za-z]+$/;
+    if (pattern.test(el.value)) {
+        // remove error class and error message if present
+        removeErrorMessage(el);
+        return el.classList.remove('error');}
+}
+
+// RENDERING TO CARD FUNCTIONS
+
+function renderCard(el, target) {
+    // check if el value is empty OR el value is < 0
+    if (el.value.trim() === '' || el.value.length < 0) return;
+
+    // remove any whitespace infront of the el value
+    el.value.trim();
+    target.textContent = '';
+
+    let text = el.value;
+    const name = el.value.split(' ');
+    let fullname;
+     
+    if (name.length === 2) {
+        text = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+        const text2 = name[1].slice(0, 1).toUpperCase() + name[1].slice(1).toLowerCase();;
+        
+        // console.log(name, name[0], name[1]);
+        // console.log(text, text2);
+        fullname = `${text} ${text2}`;
+        console.log(fullname);
+        return target.textContent = fullname;   
+    }
+
+    target.textContent = text;
+}
+
+// EVENT LISTENER FUNCTIONS
+
+function typeChecker() {
+    // check types of all input values before rendering
+    checkIfString(userName);
+    checkIfNumber(cardNumber, CARD_NUM_LEN);
+    checkIfNumber(cvc, CVC_NUM_LEN);
+    [month, year].forEach(el => checkIfNumber(el, DATE_NUM_LEN));   
+}
+
+function renderAll() {
+    // render input values when all are correct
+    renderCard(userName, cardName);
+    renderCard(cardNumber, frontCardNumber);
+    renderCard(month, cardMonth);
+    renderCard(year, cardYear);
+    renderCard(cvc, cardCVC);
+}
+
 function checkError() {
+    // check if all the input values have the error class
     let containsError = false;
-    form.forEach(el => {
+    formInputs.forEach(el => {
         if (el.classList.contains('error'))
             containsError = true;
         })
-        return containsError;
+    return containsError;
 }
 
-function init(){
+function displayCompleted() {
+    // display html after user inputs correct information
+    const html = `<div class="complete">
+      <img src="images/icon-complete.svg" alt="complete" class="complete_img">
+      <p class="complete_head">thank you! ${userName.value}</p>
+      <p class="complete_p">We've added your card details</p>
+      <button class="btn btn--reload">confirm</button>
+    </div>`
+
+    form.innerHTML = '';
+    form.insertAdjacentHTML('afterbegin', html);
+}
+
+function renderForm() {
+    const html = ``
+}
+
+function init() {
+    // clear the values of all the input fields
     input.forEach(el => {
         el.value = '';
     })
 }
 
-// Event listener that responds when user clicks and calls functions inside it
-btn.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    // Error handling function calls
-    checkIfString(userName);
-    checkIfNumber(cardNumber, CARD_NUM_LEN);
-    checkIfNumber(cvc, CVC_NUM_LEN);
-    [month, year].forEach(el => checkIfNumber(el, DATE_NUM_LEN));
+function formValidation() {
+     // Error handling function calls
+     typeChecker();
     
-    // Display texts from form on the cards
-    if (checkError() === true) return; 
-        renderCard(userName, cardName);
-        renderCard(cardNumber, frontCardNumber);
-        renderCard(month, cardMonth);
-        renderCard(year, cardYear);
-        renderCard(cvc, cardCVC);
-
-        init();
-})
+     if (checkError() === true) return; 
+     // Display texts from form on the cards
+     renderAll();
+ 
+     //display completed html
+     displayCompleted();
+     init();
+}
 
 
 // Event listener for when user starts typing
+userName.addEventListener('input', formatUsername);
 cardNumber.addEventListener('input', formatCreditCardNumber);
 month.addEventListener('input', formatNumbers);
 year.addEventListener('input', formatNumbers);
 cvc.addEventListener('input', formatNumbers);
-userName.addEventListener('input', formatUsername);
+
+
+// Event listener for submitting form and checking user info
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    formValidation();
+})
+
+// Listening for the enter key to submit form
+form.addEventListener('keyup', function (e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        formValidation();
+    }
+})
+
+btn
